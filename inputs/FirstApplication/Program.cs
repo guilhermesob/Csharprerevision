1b9b919ada01a6b5664701048d153cfcef2f84d6
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2023-06-08 00:08:48 -0300

    Add files via upload

---
+++ inputs/FirstApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NewApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*// Enter your name
+            Console.WriteLine("Type your name here:");
+            // Take input from user
+            string name = Console.ReadLine();
+            // Displaying the name on screen
+            Console.WriteLine("Your name is: " + name);*/
+
+            // Enter your id
+            Console.WriteLine("Type your id here:");
+            // Take input from user
+            int userID = Convert.ToInt32(Console.ReadLine());
+            // Displaying the id on screen
+            Console.WriteLine("Your id is: " + userID);
+        }
+    }
+}
