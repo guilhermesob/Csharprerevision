80e3185e7406afd890a7248972e14d373eaf965e
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2023-06-08 00:05:10 -0300

    Add files via upload

---
+++ type conversion/FirstApplication/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NewApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Integer to Double
+            /*           int newNum = 11;
+                       double newNum2 = newNum;
+
+                       Console.WriteLine(newNum.GetType());
+                       Console.WriteLine(newNum2.GetType());*/
+
+            // Double to integer
+            /*       double newNum3 = 11.32;
+                   int newNum4 = (int)newNum3;
+
+                   Console.WriteLine(newNum3.GetType());
+                   Console.WriteLine(newNum4.GetType());*/
+
+            // Converting types using methods
+            int newInt = 20;
+            double newDouble = 11.25;
+            bool newBool = false;
+
+            Console.WriteLine(Convert.ToString(newInt));
+            Console.WriteLine(Convert.ToDouble(newInt));
+            Console.WriteLine(Convert.ToInt32(newDouble));
+            Console.WriteLine(Convert.ToString(newBool));
+        }
+    }
+}
---
+++ variables/FirstApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NewApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string message;
+            // message = "Hello C#!";
+            // var message = "Hello C#!";
+            // message = "Hello and Welcome!";
+            // var message = "Another message"; // Error
+            int newNumber = 20;
+            Console.WriteLine(newNumber);
+        }
+    }
+}
